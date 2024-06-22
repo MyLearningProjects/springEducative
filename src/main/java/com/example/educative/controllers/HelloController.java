ee4904c250d390d170daba71d206f4cac95937aa
Chandra Gupta <chandra2009nath@gmail.com>
2024-06-22 17:42:32 +0530

    next push

--- src/main/java/com/example/educative/controllers/HelloController.java
+++ src/main/java/com/example/educative/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.educative.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +15,10 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(){
         return "hello";
+    }
+
+    @PostMapping("greeting")
+    public ResponseEntity<String> greet( @RequestParam(name = "name") String name){
+        return ResponseEntity.ok().body("hello  "  +name);
     }
 }
